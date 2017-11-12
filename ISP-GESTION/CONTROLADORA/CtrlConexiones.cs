da3adb7c309dabc6f82f1ff3229cbd69e5bd5582
eliokonzen <eliokonzen@gmail.com>
2017-11-12 17:25:17 -0300

    Conexiones

---
+++ ISP-GESTION/CONTROLADORA/CtrlConexiones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MODELO;
+namespace CONTROLADORA
+{
+    public class CtrlConexiones
+    {
+      public IList<Conexion> DBObtenerConexiones()
+        {
+            var context = SingletonContext.GetContext();
+            return context.Conexiones.ToList();
+            
+        }
+    }
+}
--- ISP-GESTION/VISTA/Conexiones/frmGestionarConexiones.cs
+++ ISP-GESTION/VISTA/Conexiones/frmGestionarConexiones.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CONTROLADORA;
 
 namespace VISTA.Conexiones
 {
     public partial class frmGestionarConexiones : Form
     {
+        private readonly CONTROLADORA.CtrlConexiones cConexiones = new CONTROLADORA.CtrlConexiones();
         public frmGestionarConexiones()
         {
             InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmGestionarConexiones_Load(object sender, EventArgs e)
+        {
+            dgvConexiones.DataSource = cConexiones.DBObtenerConexiones();
+            base.OnLoad(e);
         }
     }
 }
